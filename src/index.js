939c573cb5d9f21b6d93de5efb8d18e10bf680ef
Oleh Makarov <gstancox@gmail.com>
2018-12-19 20:14:34 +0200

    resolvers separated, auth added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+
 const links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
@@ -8,22 +13,20 @@ const links = [{
 }]
 
 const resolvers = {
-  Query: {
-    info: () => 'this is an api ',
-    feed: (root, args, context, info) => context.prisma.links(),
-    link: (parent, {id}) => links.find(l => l['id'] === id)
-  },
-  Mutation: {
-    post: (root, args, context, info) => context.prisma.createLink({
-      url: args.url,
-      description: args.description,
-    })
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const server  = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: { prisma }
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
 })
 server.start(() => console.log('server is running'))
