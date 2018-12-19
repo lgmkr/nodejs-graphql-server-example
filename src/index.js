404607e0be3306de5833c440cb4923d31c3ce7a8
Oleh Makarov <gstancox@gmail.com>
2018-12-19 15:05:20 +0200

    grapql schema pulled out, mutation added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
 const links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
@@ -22,17 +9,30 @@ const links = [{
 const resolvers = {
   Query: {
     info: () => 'this is an api ',
-    feed: () => links
+    feed: () => links,
+    link: (parent, {id}) => links.find(l => l['id'] === id)
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url
+  // Link: {
+  //   id: (parent) => parent.id,
+  //   description: (parent) => parent.description,
+  //   url: (parent) => parent.url
+  // },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${links.length++}`,
+        description: args.description,
+        url: args.url
+      }
+
+      links.push(link)
+      return link
+    }
   }
 }
 
 const server  = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 server.start(() => console.log('server is running'))
